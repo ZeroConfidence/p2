1dd698acf7cd6655e0751fb94eac9df6b23e1390
ZeroConfidence <amoldt22@student.aau.dk>
2023-04-11 00:20:14 +0200

    Update Embedder.py

--- Master/Embedder.py
+++ Master/Embedder.py
@@ -1,29 +1,36 @@
 from PIL import Image
 
+def set_lsb(n, bit):
+   
+    if bit == '1':
+        return (n | 1)
+    else:
+        return (n & ~1)
+
 
 def Carrier_embedder(Carrier,Message):
     image = Image.open(Carrier)
 
     message_binary = ''.join(format(ord(c),'08b')for c in Message)
 
-    Pixels = image.load()
+    pixels = image.load()
     pixel_count = 0
     for i in range(image.size[0]):
         for k in range(image.size[1]):
 
-            r, g, b = pixels[i, k]
+            red, green, blue = pixels[i, k]
 
             if pixel_count < len(message_binary):
-                r = set_lsb(r,(message_binary[pixel_count]))
+                red = set_lsb(red,(message_binary[pixel_count]))
                 pixel_count += 1
             if pixel_count < len(message_binary):
-                g = set_lsb(g,(message_binary[pixel_count]))
+                green = set_lsb(green,(message_binary[pixel_count]))
                 pixel_count += 1
             if pixel_count < len(message_binary):
-                b = set_lsb(b,(message_binary[pixel_count]))
+                blue = set_lsb(blue,(message_binary[pixel_count]))
                 pixel_count += 1
 
-            Pixels[i, k] = (r, g, b)
+            pixels[i, k] = (red, green, blue)
 
 
             if pixel_count >= len(message_binary):
