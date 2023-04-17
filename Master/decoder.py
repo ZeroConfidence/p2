65bee3389025efe24d229aab4cfafbdf6d28c451
ZeroConfidence <113094244+ZeroConfidence@users.noreply.github.com>
2023-04-17 13:49:50 +0200

    work in progress

--- Master/decoder.py
+++ Master/decoder.py
@@ -1,22 +1,37 @@
 from PIL import Image
 
-def Carrier_decoder(Carrier_ec):
-    image = Image.open(Carrier_ec)
+def read_lsb(n):
 
-bits = image.load()
 
-pixel_count = 0
-for i in range(image.size[0]):
-    for k in range(image.size[1]):
+def Carrier_decoder(Encrypted_image):
+    image = Image.open(Encrypted_image)
 
-        red, green, blue = bits[i, k]
+    bits = image.load()
 
-        if pixel_count < len():
-            red = set_lsb(red,([pixel_count]))
-            pixel_count += 1
-        if pixel_count < len():
-            green = set_lsb(green,([pixel_count]))
-            pixel_count += 1
-        if pixel_count < len():
-            blue = set_lsb(blue,([pixel_count]))
-            pixel_count += 1
+    pixel_count = 0
+    for i in range(image.size[0]):
+        for k in range(image.size[1]):
+            red, green, blue = bits[i, k]
+            
+            while pixel_count<8:
+                binary_length = [0]*8
+                binary_length(pixel_count) = read_lsb(red) 
+                pixel_count += 1
+                binary_length(pixel_count) = read_lsb(red) 
+                pixel_count += 1
+                if pixel_count=7:
+                    message_length = ''.join(binary_length)
+                    
+                    break
+                binary_length(pixel_count) = read_lsb(red) 
+                pixel_count += 1
+
+            if pixel_count < ():
+                red = read_lsb(red)
+                pixel_count += 1
+            if pixel_count < len():
+                green = read_lsb(green)
+                pixel_count += 1
+            if pixel_count < len():
+                blue = read_lsb(blue)
+                pixel_count += 1
