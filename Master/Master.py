411ba61e8dbbd4efed00cd1c8214263cba527bb2
Casper Gramkov Boejen <cbojen22@student.aau.dk>
2023-04-19 13:08:20 +0200

    Comments updated
    updated comments
    
    Co-Authored-By: noobstrikker <3618339+noobstrikker@users.noreply.github.com>

--- Master/Master.py
+++ Master/Master.py
@@ -1,21 +1,21 @@
-import Imagehandler
-import Embedder
-import decoder
+import Imagehandler                                                 #Imports Imagehandler file
+import Embedder                                                     #Imports Embedder file
+import Decoder                                                      #Imports Decoder file
 
 
 def ConncetionCheck(Connect):
-    if Connect == True:
+    if Connect == True:                                             #checks if the master file connected to the js translater file
         print("Master File has been connect succesfully")
-    else:
+    else:                                                           #if connection failed
         print("Master File failed to connect")
 
-def Master_Encrypt(Stockbool,Stock_Number,Importet_Image,Message):
-    if (stockbool == true):
-        Stockimage = Imagesender(Stock_Number)
-        return Carrier_embedder(Stockimage,Message)
-    else:
-        return Carrier_embedder(Importet_Image,Message)
+def Master_Encrypt(Stockbool,Stock_Number,Imported_Image,Message):  #Master encrypter calls for stockbool, Stock_number, Imported_Image and the message
+    if (stockbool == true):                                         #If the stockbool is true
+        Stockimage = Imagesender(Stock_Number)                      #then stockimage calls the imagesender file with the stock_number as an input
+        return Carrier_embedder(Stockimage,Message)                 #returns the called carrier_embedder with the stockimage and messages as inputs
+    else:                                                           #if else
+        return Carrier_embedder(Imported_Image,Message)             #returns the called carrier_embedder with the imported_image and messages as inputs
 
-def Master_Decrypt(Encrypted_image):#if you copy my homework change it a bit
-    return Carrier_decoder(Encrypted_image)
+def Master_Decrypt(Encrypted_image):                                #Master decrypted calls for the encrypted image
+    return Carrier_decoder(Encrypted_image)                         #returns the called Carrier decoder with the encrypted image as input
 
