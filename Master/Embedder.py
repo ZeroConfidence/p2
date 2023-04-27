9926c9463f44cc40e1635a0c6a71d34f955fbbb7
ZeroConfidence <amoldt22@student.aau.dk>
2023-04-27 22:00:39 +0200

    now with all new dynaics

--- Master/Embedder.py
+++ Master/Embedder.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import random
+import math
 """
 This code is designed to take a a message and encode it into a selected image using the LSB method
 It then returns the encoded image
@@ -17,8 +18,16 @@ def Carrier_embedder(Carrier,Message,key):
     image = Image.open(Carrier)
 
     message_binary = ''.join(format(ord(c),'08b')for c in Message) #converts the message into it's ASCII equivalent and then converts that into 8 bit binary and joins it into a string
+    max_bytes = ((image.size[0] * image.size[1] * 3) / 8) #finds the maximum number of encoded bytes
+    binary_length_max_bytes = math.ceil(math.log2(max_bytes)) #finds the length of the binary prepresentation of max_bytes
+
+
+    
     message_length_binary = bin(len(Message))[2:] #takes the length of message and convert in into binary and removed the '0b' prefix
-    message_length_binary = '{:0>16}'.format(message_length_binary)# pads the binary message_length_binary with 0 until it fills 16 bits
+    
+    
+    message_length_binary = message_length_binary.zfill(binary_length_max_bytes)# pads the binary message_length_binary with 0 until it fills tha same amound of bits as max_bytes
+
 
     pixels = image.load()
     Bits_encoded = 0 #used to keep track of number of encoded bits
@@ -31,36 +40,42 @@ def Carrier_embedder(Carrier,Message,key):
     for x,y in Pixel_Coordinates:
         red, green, blue = pixels[x, y]
 
-        #while loop encodes the first 16 bits with the length of the message
-        while Bits_encoded < 16:
+        #while loop encodes the first bits with the length of the message
+        while Bits_encoded < binary_length_max_bytes:
                 
             red = set_lsb(red,message_length_binary[Bits_encoded]) 
             Bits_encoded += 1
 
-            if Bits_encoded==16:
+            if Bits_encoded==binary_length_max_bytes:
                     break
 
             green = set_lsb(green,message_length_binary[Bits_encoded])
             Bits_encoded += 1
 
+            if Bits_encoded==binary_length_max_bytes:
+                    break
+
             blue = set_lsb(blue,message_length_binary[Bits_encoded]) 
             Bits_encoded += 1
 
+            if Bits_encoded==binary_length_max_bytes:
+                    break
 
-        #code does these operations only if entire message has not been encoded and not before the first 16 bits have been encoded
-        if Bits_encoded < len(message_binary+16) & Bits_encoded > 16:
-            red = set_lsb(red,(message_binary[Bits_encoded-16]))#encodes red channel for current pixel 
+
+        #code does these operations only if entire message has not been encoded and not before the length of the massage have been encoded
+        if Bits_encoded < len(message_binary+binary_length_max_bytes) & Bits_encoded > binary_length_max_bytes:
+            red = set_lsb(red,(message_binary[Bits_encoded-binary_length_max_bytes]))#encodes red channel for current pixel 
             Bits_encoded += 1
 
-        if Bits_encoded < len(message_binary+16) & Bits_encoded > 16:
-            green = set_lsb(green,(message_binary[Bits_encoded-16]))#encodes green channel for current pixel 
+        if Bits_encoded < len(message_binary+binary_length_max_bytes) & Bits_encoded > binary_length_max_bytes:
+            green = set_lsb(green,(message_binary[Bits_encoded-binary_length_max_bytes]))#encodes green channel for current pixel 
             Bits_encoded += 1
 
-        if Bits_encoded < len(message_binary+16) & Bits_encoded > 16:
-            blue = set_lsb(blue,(message_binary[Bits_encoded-16]))#encodes blue channel for current pixel 
+        if Bits_encoded < len(message_binary+binary_length_max_bytes) & Bits_encoded > binary_length_max_bytes:
+            blue = set_lsb(blue,(message_binary[Bits_encoded-binary_length_max_bytes]))#encodes blue channel for current pixel 
             Bits_encoded += 1
 
         pixels[x, y] = (red, green, blue)
 
-        if Bits_encoded >= len(message_binary+16) & Bits_encoded > 16: #once entire message is encoded returns modified image  
+        if Bits_encoded >= len(message_binary+binary_length_max_bytes) & Bits_encoded > binary_length_max_bytes: #once entire message is encoded returns modified image  
             return image
--- Master/decoder.py
+++ Master/decoder.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import random
+import math
 """
 This code is designed to take an image with an encoded message hidden inside
 Then extract the message hidden inside an return it to the master code
@@ -14,6 +15,8 @@ def Carrier_decoder(Encrypted_image,key):
 
     Bits_extracted = 0 #Used to keep track of where in the image the code is currently operating
     binary_message = [] #Used to keep the hidden message in its binary form
+    max_bytes = ((image.size[0] * image.size[1] * 3) / 8) #finds the maximum number of encoded bytes
+    binary_length_max_bytes = math.ceil(math.log2(max_bytes)) #finds the length of the binary prepresentation of max_bytes
 
     
     random.seed(key)# sets key as the seed for the psudo-random number generator 
@@ -23,14 +26,14 @@ def Carrier_decoder(Encrypted_image,key):
     for x,y in Pixel_Coordinates:
         red, green, blue = pixels[x, y]
 
-        #Checks for the first 16 bits to see how long the encoded message is
-        while Bits_extracted < 16:
-            binary_length = [0]*16
+        #Checks for the first bits to see how long the encoded message is, the number of bits checked is decided by max_bytes 
+        while Bits_extracted < binary_length_max_bytes:
+            binary_length = [0]*binary_length_max_bytes
 
             binary_length[Bits_extracted] = read_lsb(red) 
             Bits_extracted += 1
 
-            if Bits_extracted==16:
+            if Bits_extracted==binary_length_max_bytes:
                 message_length = int(''.join(map(str, binary_length)),2) #Converts the 16 binary string to int
                 break
 
@@ -41,20 +44,20 @@ def Carrier_decoder(Encrypted_image,key):
             Bits_extracted += 1
 
         #Code does these operations only if entire message has not been read, after the first 16 bits already have been read
-        if Bits_extracted < (message_length*8)+16 & Bits_extracted>16:
+        if Bits_extracted < (message_length*8)+binary_length_max_bytes & Bits_extracted>binary_length_max_bytes:
             binary_message.append(read_lsb(red))
             Bits_extracted += 1
 
-        if Bits_extracted < (message_length*8)+16 & Bits_extracted>16:
+        if Bits_extracted < (message_length*8)+binary_length_max_bytes & Bits_extracted>binary_length_max_bytes:
             binary_message.append(read_lsb(green))
             Bits_extracted += 1
 
-        if Bits_extracted < (message_length*8)+16 & Bits_extracted>16:
+        if Bits_extracted < (message_length*8)+binary_length_max_bytes & Bits_extracted>binary_length_max_bytes:
             binary_message.append(read_lsb(blue))
             Bits_extracted += 1
                 
         #Code extracts the hidden_message once the entire message has been read and returns it in text form
-        if Bits_extracted == (message_length*8)+16:
+        if Bits_extracted == (message_length*8)+binary_length_max_bytes:
             #Takes the binary message, splits it into 8 bit chunks, converts the binary chunks to int, converts the int to chr, then joins the chr to a string
             Hidden_message = ''.join([chr(int(''.join(binary_message[i:i+8]),2))for i in range(0,len(binary_message),8)])
             return Hidden_message
