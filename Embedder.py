455b1adc1465159a18e4e71a7b0085ac5466dc88
noobstrikker <nicolai.nicolai.berwald@gmail.com>
2023-05-15 14:02:19 +0200

    Add files via upload

--- Embedder.py
+++ Embedder.py
@@ -17,74 +17,96 @@ def set_lsb(n, bit): # changes the last bit of the given integer n to that of th
 
 def Carrier_embedder(Carrier,Message,key):
     image = Carrier#Image.open(Carrier)
-
+    testbool = False
     message_binary = ''.join(format(ord(c),'08b')for c in Message) #converts the message into it's ASCII equivalent and then converts that into 8 bit binary and joins it into a string
-    max_bytes = ((image.size[0] * image.size[1] * 3) / 8) #finds the maximum number of encoded bytes
+    max_bytes = ((image.size[0] * image.size[1] * 3)/8) #finds the maximum number of encoded bytes
     binary_length_max_bytes = math.ceil(math.log2(max_bytes)) #finds the length of the binary prepresentation of max_bytes
-
-
-    
+    #print("maxbytes", max_bytes)
+    #print("max lengt", binary_length_max_bytes)
+    #print("besked",message_binary)
+    #print("besked længde",len(message_binary))
     message_length_binary = bin(len(Message))[2:] #takes the length of message and convert in into binary and removed the '0b' prefix
-    
+    #print("messagelengte binær",message_length_binary)
     
     message_length_binary = message_length_binary.zfill(binary_length_max_bytes)# pads the binary message_length_binary with 0 until it fills tha same amound of bits as max_bytes
-
+    #print("efter zfill",message_length_binary)
 
     pixels = image.load()
     Bits_encoded = 0 #used to keep track of number of encoded bits
 
-    
+    #print("BLMB",binary_length_max_bytes)
     random.seed(key)# sets key as the seed for the psudo-random number generator 
     Pixel_Coordinates = [(x,y)for x in range(image.size[0]) for y in range(image.size[1])] #loads the coordinates for he pixels into the pixel_coordinate variable 
     random.shuffle(Pixel_Coordinates) #shuffles around the x and y cordinates for pixel_coordinates to make a new psudorandom sequence based on the given key
     #re-write all of this(look down)
     for x,y in Pixel_Coordinates:
         red, green, blue, alpha = pixels[x, y]
-        
+        continue_counter = 0
         #while loop encodes the first bits with the length of the message
-        while Bits_encoded < binary_length_max_bytes:
-                
+        if Bits_encoded < binary_length_max_bytes:
+            print("cordinates",x,y)
+            #print("OG-red",red)
+            #print("OG-green",green)
+            #print("OG-blue",blue)
+              
             red = set_lsb(red,message_length_binary[Bits_encoded]) 
-            print(red)
+            print("red",red)
             Bits_encoded += 1
-            print(Bits_encoded)
+            continue_counter =1
+            #print("bitNR",Bits_encoded)
             
-            if Bits_encoded==binary_length_max_bytes:
-                    break
+        if Bits_encoded < binary_length_max_bytes:    
 
             green = set_lsb(green,message_length_binary[Bits_encoded])
-            print(green)
+            print("green",green)
             Bits_encoded += 1
-            print(Bits_encoded)
+            #print("bitNR",Bits_encoded)
            
-            if Bits_encoded==binary_length_max_bytes:
-                    break
+        if Bits_encoded < binary_length_max_bytes:    
 
             blue = set_lsb(blue,message_length_binary[Bits_encoded])
-            print(blue) 
+            print("blue",blue) 
             Bits_encoded += 1
-            print(Bits_encoded)
+            #print("bitNR",Bits_encoded)
             
-            if Bits_encoded==binary_length_max_bytes:
-                    break
-            
-
-
-        #code does these operations only if entire message has not been encoded and not before the length of the massage have been encoded
+        if Bits_encoded == binary_length_max_bytes and continue_counter == 1:
+            #print("hit continue")
+            #print("check leng",len(message_binary)+binary_length_max_bytes)
+            #print("check math",Bits_encoded < len(message_binary)+binary_length_max_bytes )
+            #print("is math a lie?")
+            pixels[x, y] = (red, green, blue, alpha)
+            testbool = True
+            continue
         
-        if Bits_encoded < len(message_binary)+binary_length_max_bytes & Bits_encoded > binary_length_max_bytes:
+       
+
+        
+        #code does these operations only if entire message has not been encoded and not before the length of the massage have been encoded
+        if Bits_encoded < len(message_binary)+binary_length_max_bytes and testbool == True:
+            #print("cordinates",x,y)
+            #print("OG-red",red)
+            #print("OG-green",green)
+            #print("OG-blue",blue)
+
             red = set_lsb(red,(message_binary[Bits_encoded-binary_length_max_bytes]))#encodes red channel for current pixel 
             Bits_encoded += 1
-
-        if Bits_encoded < len(message_binary)+binary_length_max_bytes & Bits_encoded > binary_length_max_bytes:
+            #print("red",red)
+        if Bits_encoded < len(message_binary)+binary_length_max_bytes and testbool == True:
             green = set_lsb(green,(message_binary[Bits_encoded-binary_length_max_bytes]))#encodes green channel for current pixel 
             Bits_encoded += 1
+           # print("green",green)
 
-        if Bits_encoded < len(message_binary)+binary_length_max_bytes & Bits_encoded > binary_length_max_bytes:
+        if Bits_encoded < len(message_binary)+binary_length_max_bytes and testbool == True:
             blue = set_lsb(blue,(message_binary[Bits_encoded-binary_length_max_bytes]))#encodes blue channel for current pixel 
             Bits_encoded += 1
+            #print("blue",blue)
 
-        pixels[x, y] = (red, green, blue)
+        pixels[x, y] = (red, green, blue, alpha)
+    #print("msg len bin",message_length_binary)  
+
+    
+
+
 
         #if Bits_encoded >= len(message_binary+binary_length_max_bytes) & Bits_encoded > binary_length_max_bytes: #once entire message is encoded returns modified image  
     return image
--- decoder.py
+++ decoder.py
@@ -12,51 +12,85 @@ def Carrier_decoder(Encrypted_image,key):
     image = Encrypted_image#Image.open(Encrypted_image)
 
     
-
+    pixels = image.load()
     Bits_extracted = 0 #Used to keep track of where in the image the code is currently operating
     binary_message = [] #Used to keep the hidden message in its binary form
-    max_bytes = ((image.size[0] * image.size[1] * 3) / 8) #finds the maximum number of encoded bytes
+    max_bytes = ((image.size[0] * image.size[1] * 3)/8) #finds the maximum number of encoded bytes
     binary_length_max_bytes = math.ceil(math.log2(max_bytes)) #finds the length of the binary prepresentation of max_bytes
     
-    
+    print("max",max_bytes,"bin_len_max bytes",binary_length_max_bytes)
     random.seed(key)# sets key as the seed for the psudo-random number generator 
     Pixel_Coordinates = [(x,y)for x in range(image.size[0]) for y in range(image.size[1])] #loads the coordinates for he pixels into the pixel_coordinate variable 
     random.shuffle(Pixel_Coordinates) #shuffles around the x and y cordinates for pixel_coordinates to make a new psudorandom sequence based on the given key
-    
+    message_length_bits=""
     for x,y in Pixel_Coordinates:
-        red, green, blue,alpha = image.getpixel((x,y))
+        red, green, blue,alpha = pixels[x,y]
         
         #Checks for the first bits to see how long the encoded message is, the number of bits checked is decided by max_bytes 
         if Bits_extracted < binary_length_max_bytes:
-            red_bit=read_lsb(red)
-            green_bit=read_lsb(green)
-            blue_bit=read_lsb(blue)
-            message_length_bits=[]
-            message_length_bits.extend([red_bit,green_bit,blue_bit])
-            Bits_extracted += 3
-            if Bits_extracted >= binary_length_max_bytes:
-                message_length = int(''.join(map(str,message_length_bits[:binary_length_max_bytes])),2)
+            print("cords",x,y)
+            print("red",red)
+            print("green",green)
+            print("blue",blue)
+            message_length_bits = message_length_bits + str(read_lsb(red))
+            print("red-read",read_lsb(red))
+            print(message_length_bits)
+            Bits_extracted += 1
+            print ("bitNR",Bits_extracted)
 
-    message_bits = []
-    for x,y in Pixel_Coordinates:
-        if x == 0 and y == 0:
-            continue
-        if len(message_bits) >= message_length:
-            break
+        if Bits_extracted < binary_length_max_bytes:    
+            message_length_bits = message_length_bits + str(read_lsb(green))
+            print("green-read",read_lsb(green))
+            print(message_length_bits)
+            Bits_extracted += 1
+            print ("bitNR",Bits_extracted)
+
+        if Bits_extracted < binary_length_max_bytes:    
+            message_length_bits = message_length_bits + str(read_lsb(blue))
+            print("blue-read",read_lsb(blue))
+            print(message_length_bits)
+            Bits_extracted += 1
+            print ("bitNR",Bits_extracted)
+            
+    print("message_length_bits",message_length_bits)       
         
-        red,green,blue,alpha = image.getpixel((x,y))
-        red_bit = read_lsb(red)
-        green_bit = read_lsb(green)
-        blue_bit = read_lsb(blue)
-        message_bits.extend([red_bit,green_bit,blue_bit])
+        
+            
+          
+    message_length = int(message_length_bits,2)*8            
+    print("msg len",message_length)
+    message_bits = []
+    Bits_extracted = 0
+    for x,y in Pixel_Coordinates:
+        if  Bits_extracted<binary_length_max_bytes:  
+            if x == 0 and y == 0:
+                continue
+            if len(message_bits) >= message_length:
+                break
+            
+            red,green,blue,alpha = image.getpixel((x,y))
+            if  Bits_extracted<binary_length_max_bytes: 
+                red_bit = read_lsb(red)
+                message_bits.extend([red_bit,green_bit,blue_bit])
+            Bits_extracted += 1
+            if  Bits_extracted<binary_length_max_bytes:     
+                green_bit = read_lsb(green)
+            Bits_extracted += 1
+            if  Bits_extracted<binary_length_max_bytes:     
+                blue_bit = read_lsb(blue)
+            Bits_extracted += 1
+            
         
         #Code does these operations only if entire message has not been read, after the first 16 bits already have been read
-    
+        
                 
         #Code extracts the hidden_message once the entire message has been read and returns it in text form
-    
+    print("messagebits",message_bits)
+    #print("message lengyh",message_length)
+    #print("len",len(message_bits))
             #Takes the binary message, splits it into 8 bit chunks, converts the binary chunks to int, converts the int to chr, then joins the chr to a string
     Hidden_message = ''.join([chr(int(''.join(message_bits[i:i+8]),2))for i in range(0,len(message_bits),8)])
             #Hidden_message = Hidden_message.replace('\x00','') + chr('a')
     print(Hidden_message)  
+
     return Hidden_message #f"redpixelvalue:{red}(binary:{bin(red)})"
